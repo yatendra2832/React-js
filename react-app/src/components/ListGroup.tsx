e1fc9addb8655301fe92766d73e69a2f03773b70
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-02 15:50:16 +0530

    rendering a list

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,19 @@
 // import { Fragment } from "react"; <fragment></fragment> we can also use that ype fragment or another way is using the <></> empty angular bracket
 
 function ListGroup() {
+  const items = ["Item  1", "Item  2", "Item  3", "Item  4", "Item  5"];
   return (
     <>
       <h1>Lists of Item</h1>
       <ul className="list-group">
-        <li className="list-group-item">Book</li>
+        {/* <li className="list-group-item">Book</li>
         <li className="list-group-item">Pen</li>
         <li className="list-group-item">Pencil</li>
         <li className="list-group-item">Rubber</li>
-        <li className="list-group-item"> Shrapner</li>
+        <li className="list-group-item"> Shrapner</li> */}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
