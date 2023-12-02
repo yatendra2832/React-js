ba86436928aae4cc0c3016674e2aab7eec6d53f4
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-02 16:04:06 +0530

    conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
 // import { Fragment } from "react"; <fragment></fragment> we can also use that ype fragment or another way is using the <></> empty angular bracket
 
 function ListGroup() {
-  const items = ["Item  1", "Item  2", "Item  3", "Item  4", "Item  5"];
+  let items = ["Item  1", "Item  2", "Item  3", "Item  4", "Item  5"];
+  items = [];
+
   return (
     <>
       <h1>Lists of Item</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && "No item found"}
       <ul className="list-group">
         {/* <li className="list-group-item">Book</li>
         <li className="list-group-item">Pen</li>
