a54bd99b4973efb0f482eed3a168d81d66309866
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-02 19:56:11 +0530

    children props and using alert in react js

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/alert";
 
 function App() {
-  let items = ["Item  1", "Item  2", "Item  3", "Item  4", "Item  5"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Items"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World !</Alert>
     </div>
   );
 }
