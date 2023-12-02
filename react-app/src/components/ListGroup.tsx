455c229730e30ca25327588f7575881081202fa0
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-02 16:14:36 +0530

    Event handling in react

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,22 +1,23 @@
 // import { Fragment } from "react"; <fragment></fragment> we can also use that ype fragment or another way is using the <></> empty angular bracket
 
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Item  1", "Item  2", "Item  3", "Item  4", "Item  5"];
-  items = [];
+
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>Lists of Item</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
       {items.length === 0 && "No item found"}
       <ul className="list-group">
-        {/* <li className="list-group-item">Book</li>
-        <li className="list-group-item">Pen</li>
-        <li className="list-group-item">Pencil</li>
-        <li className="list-group-item">Rubber</li>
-        <li className="list-group-item"> Shrapner</li> */}
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
