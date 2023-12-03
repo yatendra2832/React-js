3e0855bd2ccd79205c506d4805d6013a34d6bfa8
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-03 13:27:07 +0530

    Restrict mode in the react js

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,9 +3,6 @@ import Message from "./components/Message";
 function App() {
   return (
     <div>
-      
-      <Message />
-      <Message />
       <Message />
     </div>
   );
--- react-app/src/components/Message.tsx
+++ react-app/src/components/Message.tsx
@@ -1,7 +1,9 @@
+let count = 0;
+
 const Message = () => {
-  let count = 0; //when we take initialization into then it will not re rendering
-  count++; // that will make the function to be re-rendered every time and impure
+  console.log("Message Called ", count);
+  count++;
   return <div>Message {count}</div>;
 };
-
+// in strict mode our components will render twice that is the reason why we are seeing the count is to message 2 in the screen as in the production mode it will render only once as we will not using the restrict mode
 export default Message;
