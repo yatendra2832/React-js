930ec3b677c11d577021a835a7799704bf889a86
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-03 13:55:48 +0530

    update the array

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "yatendra singh",
-    address: {
-      city: "tundla",
-      zipcode: 283204,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerfull", "excited"]);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipcode: 283205 },
-    });
-    console.log(customer);
+    // we want when the button is clicked them the new item will be add to the array
+    // add
+    setTags([...tags, "greetings"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== "excited"));
+
+    // update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+
+    console.log(tags);
   };
   return (
     <div>
-      <button onClick={handleClick}>Update Address</button>
+      <button onClick={handleClick}>Update Tags</button>
     </div>
   );
 }
