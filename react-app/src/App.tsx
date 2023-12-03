1e9fe007034e04ac947f3ab086a3335cfe9d27c3
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-04 00:01:05 +0530

    Expandable Text

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["product1", "product2"]);
-
   return (
     <div>
-      <Navbar cartItemsCount={cartItems.length} />
-      <Cart
-        cartItems={cartItems}
-        onClear={() => setCartItems([])}
-        // onRemove={() => setCartItems([])}
-      />
+      <ExpandableText maxChars={30}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        consectetur iste placeat, facilis, vel eius odio officia perspiciatis
+        laborum esse, eum vero. Sint, reprehenderit quam nihil possimus Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Sunt in rerum iusto,
+        fuga quas reiciendis repellendus dolorem minima cupiditate obcaecati
+        aliquam quaerat dolore ducimus maxime, sequi qui doloribus aut autem
+        unde perferendis! Ab qui itaque repellat quaerat corrupti aliquam
+        commodi. veritatis aperiam illo!
+      </ExpandableText>
     </div>
   );
 }
