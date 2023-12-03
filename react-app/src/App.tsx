23c538dd470c341737d0ef9b40a7c72e0dcbf431
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-03 13:41:13 +0530

    updating object onclick will update the price

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,21 @@
-import Message from "./components/Message";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Coco Cola",
+    price: 35,
+  });
+
+  const handleClick = () => {
+    setDrink({
+      ...drink,
+      price: drink.price + 5,
+    });
+    console.log(drink.price);
+  };
   return (
     <div>
-      <Message />
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
