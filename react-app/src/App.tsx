7c99cb2aa19c178a931c303f29235910944e659e
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-05 20:55:18 +0530

    creating form using the react

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={30}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        consectetur iste placeat, facilis, vel eius odio officia perspiciatis
-        laborum esse, eum vero. Sint, reprehenderit quam nihil possimus Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sunt in rerum iusto,
-        fuga quas reiciendis repellendus dolorem minima cupiditate obcaecati
-        aliquam quaerat dolore ducimus maxime, sequi qui doloribus aut autem
-        unde perferendis! Ab qui itaque repellat quaerat corrupti aliquam
-        commodi. veritatis aperiam illo!
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
