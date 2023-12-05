0bf94b3b31202d301b62ca88f9c5f1d4b4af3c86
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-05 21:15:58 +0530

    useref in react js

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("submitted successfully");
+    if (nameRef.current !== null) {
+      // console.log(nameRef.current.value);
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      // console.log(ageRef.current.value);
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log(person)
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,17 +23,18 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" id="name" className="form-control" />
+        <input ref={nameRef} type="text" id="name" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input type="number" id="age" className="form-control" />
+        <input ref={ageRef} type="number" id="age" className="form-control" />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
+// In React.js, useRef is a Hook that provides a way to access and interact with the DOM (Document Object Model) directly. It is particularly useful when you need to reference a DOM element or keep a mutable object persisting across renders without causing the component to re-render.
 
 export default Form;
