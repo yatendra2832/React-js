4f349a784c55dc6b2d183b501470eb72b83061f2
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-07 08:27:16 +0530

    expenselist using in app but passing array

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,23 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/component/ExpenseList";
 
+// here we are passing the expenses  array where as later we will setting the data by collecting the data from the form and sending it to the server
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Food", amount: 100, category: "Food" },
+    { id: 2, description: "Food", amount: 10, category: "Food" },
+    { id: 3, description: "Food", amount: 180, category: "Food" },
+    { id: 4, description: "Food", amount: 100, category: "Food" },
+  ]);
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
 
+//now when we are clicking on the delete button the following function will be called and the id will be passed as a parameter and the expenses will be filtered
 export default App;
