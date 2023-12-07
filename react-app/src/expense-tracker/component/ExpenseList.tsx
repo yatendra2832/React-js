bec07d8c08da7f64cfe12f5ee96e7253a02f7c71
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-07 08:26:40 +0530

    expense list for expense tracker project

---
+++ react-app/src/expense-tracker/component/ExpenseList.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === 0) return null; // this is for when there is no data in the array we will return null and no table will be at the website
+  return (
+    <table className="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            Rs.{" "}
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ExpenseList;
