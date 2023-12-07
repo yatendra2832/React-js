5bb512303bdd59b641132e52dc7fba1b9963a778
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-07 08:43:03 +0530

    expense filter according to the category

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/component/ExpenseList";
+import ExpenseFilter from "./expense-tracker/component/ExpenseFilter";
 
 // here we are passing the expenses  array where as later we will setting the data by collecting the data from the form and sending it to the server
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Food", amount: 100, category: "Food" },
-    { id: 2, description: "Food", amount: 10, category: "Food" },
+    { id: 2, description: "Groceries ", amount: 10, category: "Groceries" },
     { id: 3, description: "Food", amount: 180, category: "Food" },
-    { id: 4, description: "Food", amount: 100, category: "Food" },
+    { id: 4, description: "Groceries ", amount: 100, category: "Groceries" },
+    {
+      id: 5,
+      description: "Entertaniment",
+      amount: 100,
+      category: "Entertainment",
+    },
+    { id: 6, description: "utilities", amount: 100, category: "Utilities" },
+    { id: 7, description: "utilities", amount: 100, category: "Utilities" },
+    { id: 8, description: "Groceries ", amount: 100, category: "Groceries" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
 }
 
-//now when we are clicking on the delete button the following function will be called and the id will be passed as a parameter and the expenses will be filtered
 export default App;
---
+++ react-app/src/expense-tracker/component/ExpenseFilter.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(e) => onSelectCategory(e.target.value)}
+    >
+      <option value="">All Categories</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;
