520bc5791ab06e27154d44b86b77040bb42aa370
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-10 09:20:54 +0530

    UseEffect  for handling side effects in the reactjs

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,36 +1,23 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/component/ExpenseList";
-import ExpenseFilter from "./expense-tracker/component/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/component/ExpenseForm";
-import categories from "./expense-tracker/categories";
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    // { id: 4, description: "Groceries ", amount: 100, category: "Groceries" },
-    { id: 1, description: "home ", amount: 100, category: "Groceries" },
-  ]);
+import { useEffect, useRef } from "react";
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+function App() {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  });
+  useEffect(() => {
+    document.title = "React js ";
+  });
+
   return (
     <div>
-      <div className="mb-3">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      <input
+        ref={ref}
+        type="text"
+        className="form-control"
+        placeholder="Name"
       />
     </div>
   );
