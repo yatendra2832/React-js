54f0903cd96d4866324c3a421d7d0f9b74cba3df
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-10 21:52:09 +0530

    update Users method by put and patch

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -53,12 +53,23 @@ function App() {
         setUsers(users.filter((u) => u.id !== newUser.id));
       });
   };
+  const updateUser = (user: Users) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "@gmail.com" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios
+      .put(`https://jsonplaceholder.typicode.com/users/${user.id}`, updatedUser)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <div>
       {error && <p className="text-danger fw-bold fs-3">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
       <ul className="list-group">
-        <button className="btn btn-primary mb-3   w-25" onClick={addUser}>
+        <button className="btn btn-primary mb-3 w-25" onClick={addUser}>
           Add
         </button>
         {users.map((user) => (
@@ -67,14 +78,24 @@ function App() {
             className="list-group-item  d-flex justify-content-between "
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => {
-                deleteUser(user);
-              }}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => {
+                  updateUser(user);
+                }}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger mx-2"
+                onClick={() => {
+                  deleteUser(user);
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
