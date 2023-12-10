4388d512d558bf2ffa6df623c2a42fe6cc997ac7
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-10 18:12:18 +0530

    Deleting the data using axios.delete

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -31,13 +31,36 @@ function App() {
     };
   }, []);
 
+  const deleteUser = (user: Users) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/xusers/${user.id}`)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <div>
       {error && <p className="text-danger fw-bold fs-1">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <div key={user.id}>{user.name}</div>
+          <li
+            key={user.id}
+            className="list-group-item  d-flex justify-content-between "
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => {
+                deleteUser(user);
+              }}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
